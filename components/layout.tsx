0d03c61a3f5ca0d00add4c41fd4b7e4ad01c1a1d
Aliaksandr <alex.borodach@gmail.com>
2023-05-15 11:30:42 +0200

    Setup app layout

---
+++ components/layout.tsx
@@ -0,0 +1,17 @@
+import { Inter } from "next/font/google";
+import clsx from "clsx";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <main className={clsx("flex h-screen flex-col", inter.className)}>
+      <nav className="fixed top-0 z-50 w-full border-b border-base-300">
+        <div className="px-3 py-3 lg:px-5">
+          <div className="flex items-center justify-end">Navbar</div>
+        </div>
+      </nav>
+      <div className="pt-16 flex-1 container mx-auto">{children}</div>
+    </main>
+  );
+}
