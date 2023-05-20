f7365ceec2784625e9aefb70e5d7164846b62364
Aliaksandr <alex.borodach@gmail.com>
2023-05-20 12:36:54 +0200

    Add WS support for Apollo server Run prettier

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,14 +1,26 @@
 import "reflect-metadata";
 import path from "path";
-import Cors from 'cors';
-import {ApolloServer} from 'apollo-server-micro';
-import { buildSchema, Authorized, MiddlewareInterface, ResolverData, NextFn, UseMiddleware } from 'type-graphql';
-import { NextApiRequest, NextApiResponse } from 'next';
-import {jwtVerify} from 'jose';
+import Cors from "cors";
+import { ApolloServer } from "apollo-server-micro";
+import {
+  buildSchema,
+  Authorized,
+  MiddlewareInterface,
+  ResolverData,
+  NextFn,
+  UseMiddleware,
+} from "type-graphql";
+import { NextApiRequest, NextApiResponse } from "next";
+import { jwtVerify } from "jose";
+import { useServer } from "graphql-ws/lib/use/ws";
+import { WebSocketServer } from "ws";
+import Redis, { RedisOptions } from "ioredis";
+import { RedisPubSub } from "graphql-redis-subscriptions";
 
 import { PrismaClient } from "@prisma/client";
 
 import { resolvers, applyResolversEnhanceMap, User } from "~/prisma/generated/type-graphql";
+import { CustomUserBooksResolver } from "~/graphql/resolvers";
 
 const cors = Cors();
 
@@ -24,7 +36,6 @@ function runMiddleware(req: NextApiRequest, res: NextApiResponse, fn: typeof cor
   });
 }
 
-
 interface Context {
   user: User | null;
   prisma: PrismaClient;
@@ -33,16 +44,15 @@ interface Context {
 export class CheckBookAccess implements MiddlewareInterface<Context> {
   async use({ context, args }: ResolverData<Context>, next: NextFn) {
     const book = await context.prisma.userBook.findUnique({ where: args.where });
-    console.log(context.user);
     if (book?.userId !== context.user?.id) {
-      throw new Error('Unauthorized');
+      throw new Error("Unauthorized");
     }
     return next();
   }
 }
 
 export async function verify(token: string, secret: string) {
-  const {payload} = await jwtVerify(token, new TextEncoder().encode(secret));
+  const { payload } = await jwtVerify(token, new TextEncoder().encode(secret));
   return payload;
 }
 
@@ -59,11 +69,23 @@ applyResolversEnhanceMap({
   },
 });
 
+const options: RedisOptions = {
+  host: "127.0.0.1",
+  port: 6379,
+  retryStrategy: (times) => Math.max(times * 100, 3000),
+};
+
+const pubSub = new RedisPubSub({
+  publisher: new Redis(options),
+  subscriber: new Redis(options),
+});
+
 const schema = await buildSchema({
-  resolvers,
+  resolvers: [...resolvers, CustomUserBooksResolver],
   emitSchemaFile: path.resolve("./graphql/schema.graphql"),
   validate: false,
-  authChecker: () => true,
+  pubSub,
+  authChecker: ({ context }: ResolverData<Context>) => !!context.user,
 });
 
 const prisma = new PrismaClient();
@@ -71,24 +93,47 @@ const prisma = new PrismaClient();
 const apolloServer = new ApolloServer({
   schema,
   context: async ({ req }: { req: NextApiRequest }): Promise<Context> => {
-    const token = req.cookies['authToken'] || req.headers.authorization || '';
-    const user = await verify(token, 'secret') as unknown as User;
-    return { prisma, user }
+    const token = req.cookies["authToken"] || req.headers.authorization || "";
+    const user = (await verify(token, "secret")) as unknown as User;
+    return { prisma, user };
   },
+  plugins: [
+    {
+      async serverWillStart(...asdf) {
+        return {
+          async drainServer() {
+            if (graphqlWSS) {
+              await graphqlWSS.dispose();
+            }
+          },
+        };
+      },
+    },
+  ],
 });
- 
+
 export const config = {
-  api: {bodyParser: false},
+  api: { bodyParser: false },
 };
 
 const startServer = apolloServer.start();
+let graphqlWSS: any;
+let apolloHandler: ReturnType<ApolloServer["createHandler"]>;
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
+let wsServer: WebSocketServer;
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (!apolloHandler) {
+    wsServer = new WebSocketServer({
+      // @ts-ignore
+      server: res.socket.server,
+      path: "/api/graphql",
+    });
+    graphqlWSS = useServer({ schema }, wsServer);
+    await startServer;
+    apolloHandler = apolloServer.createHandler({ path: "/api/graphql" });
+  }
   await runMiddleware(req, res, cors);
   await prisma.$connect();
-  await startServer;
-  await apolloServer.createHandler({ path: '/api/graphql' })(req, res);
+  await apolloHandler(req, res);
 }
